cb69765149690da06619d9031c4007326517f6ce
Bajaj1084 <103529195+Bajaj1084@users.noreply.github.com>
2025-02-03 14:45:45 +0530

    Update V1.1.1__DB_SCHEMA_CREATION.sql

--- Setup/V1.1.1__DB_SCHEMA_CREATION.sql
+++ Setup/V1.1.1__DB_SCHEMA_CREATION.sql
@@ -2,4 +2,4 @@ CREATE DATABASE IF NOT EXISTS procedure_db;
 use database procedure_db;
 CREATE schema IF NOT EXISTS procedure_db.procedure_schema;
 use schema procedure_schema;
-CREATE TABLE temp;
+CREATE TABLE temp1;
